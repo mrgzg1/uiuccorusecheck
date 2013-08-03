1f091da2a546f56ddd5cab749c5ebdf67ffacfe8
mrgzg1 <mrgzg1@gmail.com>
2013-08-03 15:57:18 -0400

    first commit
    hopefully the last too.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ uiuccorusecheck
 ===============
 
 check if a uiuc course is available
+
+
+just update the config.py with relevant value and run the script in bg on your server or desktop. It'll send you an email when it finds any of the course in your list open for registration.
+
+adapted from: smiley325/uiucregistrationtools
---
+++ scrapper.py
@@ -0,0 +1,121 @@
+from BeautifulSoup import BeautifulSoup
+from config import *
+import mechanize
+import getpass
+import time
+import smtplib  
+
+def email(subj_code, subj_no, crn_no):
+    #http://www.pythonforbeginners.com/code-snippets-source-code/using-python-to-send-email/
+    header  = 'From: %s\n' % gmail[0]
+    header += 'To: %s\n' % ','.join(email_to)
+    header += 'Cc: %s\n' % ','.join([])
+    header += 'Subject: COURSE REGISTRATION!!!\n\n'
+    message = header + "REGISTER FAST!!! "+subj_code+" "+str(subj_no)+" "+str(crn_no)+" is available!" 
+  
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(gmail[0],gmail[1])
+    problems = server.sendmail(gmail[0], email_to, message)
+    server.quit()
+
+def login(br):
+    #go past the login page
+    br.set_handle_robots(False)
+    url = "https://eas.admin.uillinois.edu/eas/servlet/EasLogin?redirect=https://webprod.admin.uillinois.edu/ssa/servlet/SelfServiceLogin?appName=edu.uillinois.aits.SelfServiceLogin&dad=BANPROD1"
+    br.open(url)
+    br.select_form(name="easForm")
+    br["inputEnterpriseId"] = enterprise[0]
+    br["password"] = enterprise[1]
+    resp = br.submit()
+
+
+def get_course_table(br, subj_code, subj_no):
+    #go to the registration page
+    url = "https://ui2web1.apps.uillinois.edu/BANPROD1/twbkwbis.P_GenMenu?name=bmenu.P_StuMainMnu"
+    br.open(url)
+    url = "https://ui2web1.apps.uillinois.edu/BANPROD1/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu"
+    br.open(url)
+    url = "https://ui2web1.apps.uillinois.edu/BANPROD1/twbkwbis.P_GenMenu?name=bmenu.P_RegAgreementAdd"
+    br.open(url)
+    url = "https://ui2web1.apps.uillinois.edu/BANPROD1/bwskfreg.P_AltPin" #fall 2013 selection page
+    br.open(url)
+    br.select_form(nr=1)
+    br.submit()
+
+    #on the reg page
+    br.select_form(nr=1)
+    resp = br.submit(name='REG_BTN', label='Class Search')
+
+    br.select_form(nr=1)
+    resp = br.submit(name='SUB_BTN', label = 'Advanced Search')
+
+    br.select_form(nr=1)
+    br.find_control(name="sel_subj", nr=1).value = [subj_code]
+    br["sel_crse"] = R_COURSE = str(subj_no)
+    resp = br.submit().read()
+
+    f_name = str(subj_code)+"_"+str(subj_no)+".txt"
+    f2 = open("raw_"+f_name, "w")
+    f2.write(resp)
+    f2.close()
+
+    return resp
+
+def parse_page(resp, crn_no):
+    soup = BeautifulSoup(resp)
+    table = soup.findAll("table",{"class":"datadisplaytable"})
+    if(len(table) == 0):
+        append_to_log("Can't find the course table")
+        return False
+    table = table[0]
+
+    crn_nav_txt = table.find(text=str(crn_no))
+    if crn_nav_txt is None:
+        append_to_log("Can't find the CRN "+str(crn_no))
+        return False
+
+    crn_td = crn_nav_txt.parent.parent
+    avail_td = crn_td.findPreviousSibling("td")
+
+    if(avail_td.find("input") is not None):
+        return True
+    return False
+
+def check_course(br, subj_code, subj_no, crn_no):
+    resp = get_course_table(br, subj_code, subj_no)
+    if parse_page(resp, crn_no):
+        text = subj_code+" "+str(subj_no)+" is available"
+        append_to_log(text)
+        email(subj_code, subj_no, crn_no) #email the user
+    else:
+        text = subj_code+" "+str(subj_no)+" is un-available"
+        append_to_log(text)
+
+def run_routine():
+    br = mechanize.Browser()
+    login(br)
+    for each in courses:
+        check_course(br, each[0], each[1], each[2])
+    br.close()
+
+def append_to_log(text):
+    f = open(log_file, 'a')
+    text = time.ctime()+":"+text+"\n"
+    f.write(text)
+    f.close()
+
+def check_log_file():
+    try:
+       with open(log_file): pass
+    except IOError:
+       f = open(log_file, 'w')
+       f.write("created on:"+time.ctime()+"\n")
+       f.close()
+
+check_log_file()
+while(1):
+    append_to_log("running check")
+    run_routine()
+    time.sleep(time_interval)
+    
